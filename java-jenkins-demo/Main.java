61d82537aa53cd579d363370bbf369ea13b8fd72
sammed-18 <149004008+sammed-18@users.noreply.github.com>
2024-11-08 10:07:52 +0530

    Update Main.java

--- java-jenkins-demo/Main.java
+++ java-jenkins-demo/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Sammed and Jenkins!");
+        //added comment to test jenkins
         
     }
 }
